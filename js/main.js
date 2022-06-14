8302e4af27672e3f76565db9599c2dfebf7b26a7
Yaroslav-627 <yarslavpopovych@gmail.com>
2022-06-14 22:18:31 +0300

    full frontend

---
+++ js/main.js
@@ -0,0 +1,21 @@
+let you;
+let yourScore;
+let opponent;
+let opponentScore;
+let choices = ["scissors", "rock", "paper"];
+
+let img__opponent = document.querySelector(".opponent__img");
+let your__img = document.querySelector(".your__img");
+let imgChoices = document.querySelector(".img__choices");
+
+function opponentChoice() {
+    let choice = Math.floor(Math.random() * 2.9);
+    console.log(choice);
+    img__opponent.src = "js/" + choices[choice] + ".png";
+}
+function yourChoice() {
+    your__img.src = img__opponent.src;
+}
+
+opponentChoice();
+yourChoice()
