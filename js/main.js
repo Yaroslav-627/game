0171aa82b719b129043919c5ef8ff070c2688f98
Yaroslav-627 <yarslavpopovych@gmail.com>
2022-06-30 16:48:44 +0300

    base mechanic

--- js/main.js
+++ js/main.js
@@ -1,17 +1,21 @@
 let you;
-let yourScore;
 let opponent;
-let opponentScore;
+let yourScore = 0;
+let opponentScore = 0;
+let drawScore = 0;
+let data = [];
 let choices = ["scissors", "rock", "paper"];
 
 let img__opponent = document.querySelector(".opponent__img");
 let your__img = document.querySelector(".your__img");
+let opponentText = document.querySelector(".opponent__score");
+let yourText = document.querySelector(".your__score");
 let uChoices = document.querySelectorAll("[data-choices]");
 
-function opponentChoice() {
+function opponentChoice(scr) {
     let choice = Math.floor(Math.random() * 2.9);
-    console.log(choice);
     img__opponent.src = "img/" + choices[choice] + ".png";
+    Score(scr, choices[choice]);
 }
 uChoices.forEach(uChoice => {
     uChoice.addEventListener("click", e =>{
@@ -21,6 +25,27 @@ uChoices.forEach(uChoice => {
 })
 function Selection(selection){
     your__img.src = "img/" + selection + ".png";
-    opponentChoice();
+    opponentChoice(selection);
+    render(selection);
 }
-imgChoices.addEventListener("click", yourChoice);
+function Score(your, oppo) {
+    if(your == "rock" && oppo == "scissors" || your == "scissors" && oppo == "paper" || your == "paper" && oppo == "rock"){
+        console.log(oppo, your);
+        yourScore++;
+    }else if(your == "rock" && oppo == "paper" || your == "paper" && oppo == "scissors" || your == "scissors" && oppo == "rock"){
+        console.log(oppo, your);
+        opponentScore++;
+    }else{
+        console.log(oppo, your);
+        drawScore++;
+    }
+}
+function render(you) {
+    // data.push({your_choices: you, PC_choices: your__img.src, who_won: your__img.src});
+    // console.log(data);
+    // const stor = window.localStorage;
+    // stor.setItem("data", data);
+    opponentText.innerText = "Computer Score: " + opponentScore + ";";
+    yourText.innerText = "Your Score: " + yourScore + ";";
+    console.log(drawScore + "; " + opponentScore, yourScore);
+}
